da8854f1fb8cc091e6479068017f5ed87a967a7b
KseniaRumyantseva <exoplanet6705@gmail.com>
2022-12-12 00:34:46 +0500

    update all files

--- app.py
+++ app.py
@@ -11,11 +11,7 @@ app.config["JWT_SECRET_KEY"] = "super-secret"
 jwt = JWTManager(app)
 
 
-@app.route("/")
-def hello_world():
-    return "Hello World!"
-
-
+# регистрация пользователя
 @app.route("/reg", methods=['POST'])
 def register():
     login = str(request.json.get("login", None))
@@ -27,6 +23,7 @@ def register():
         return make_response('Не удалось зарегистрировать пользователя')
 
 
+# авторизация пользователя
 @app.route("/auth", methods=['POST'])
 def authorization():
     login = str(request.json.get("login", None))
@@ -39,6 +36,7 @@ def authorization():
         return make_response('Неверный логин или пароль')
 
 
+# добавление сокращенной ссылки
 @app.route("/add_link", methods=['POST'])
 @jwt_required()
 def add_link():
@@ -58,7 +56,8 @@ def add_link():
         return make_response("Введите ссылку")
 
 
-@app.route("/list_links", methods=['POST'])
+# просмотр списка ссылок, которые добавил пользователь
+@app.route("/list_links", methods=['GET'])
 @jwt_required()
 def list_users_links():
     login = str(get_jwt_identity())
@@ -68,6 +67,7 @@ def list_users_links():
         return make_response("У вас пока нет добавленных ссылок")
 
 
+# удаление выбранной ссылки
 @app.route("/del_link", methods=['POST'])
 @jwt_required()
 def delete():
@@ -80,9 +80,10 @@ def delete():
         return make_response("Не удалось удалить ссылку")
 
 
+# обновление имени выбранной ссылки
 @app.route("/update_link", methods=['POST'])
 @jwt_required()
-def update():
+def update_link():
     login = str(get_jwt_identity())
     old_link = str(request.json.get("old_link", None))
     new_link = str(request.json.get("new_link", None))
@@ -97,14 +98,55 @@ def update():
         return make_response("Не удалось обновить ссылку")
 
 
+# изменение уровня доступа выбранной ссылки
+@app.route("/update_access", methods=['POST'])
+@jwt_required()
+def update_access():
+    login = str(get_jwt_identity())
+    link = str(request.json.get("link", None))
+    new_access = str(request.json.get("new_access", None))
+    if link != '' and (new_access == '1' or new_access == '2' or new_access == '3'):
+        functions.update_access_from_db(login, link, new_access)
+        return make_response("Уровень доступа успешно изменен")
+    else:
+        return make_response("Не удалось изменить уровень доступа")
+
+
+# подсчет переходов по конкретной ссылки
 @app.route("/<short_link>")
 def redirect(short_link):
-    return str(f'Количество переходов по этой ссылке - {functions.redirect_db(short_link)}')
+    if functions.check_access(short_link) == 1:
+        return str(f'Количество переходов по этой ссылке - {functions.redirect_db(short_link)}')
+    elif functions.check_access(short_link) == 2:
+        return check_3(short_link)
+    elif functions.check_access(short_link) == 3:
+        return check_2(short_link)
+    else:
+        return make_response("Ошибка перехода по ссылке")
+
+
+# проверка условий для подсчета переходов по ссылки, если уровень доступа - 'private'
+@jwt_required()
+def check_3(short_link):
+    login = str(get_jwt_identity())
+    if functions.check_owner_link(login, short_link):
+        return str(f'Количество переходов по этой ссылке - {functions.redirect_db(short_link)}')
+    else:
+        return make_response("Вы не являетесь владельцем этой ссылки, поэтому не можете перейти по ней")
+
+
+# проверка условий для подсчета переходов по ссылки, если уровень доступа - 'protected'
+@jwt_required()
+def check_2(short_link):
+    login = str(get_jwt_identity())
+    if functions.check_user_is_correct(login) is not None:
+        return str(f'Количество переходов по этой ссылке - {functions.redirect_db(short_link)}')
+    else:
+        return make_response("Вы не авторизовались")
 
 
 if __name__ == '__main__':
     app.run()
-
 
 # user7 - 111
 # user8 - 888
--- functions.py
+++ functions.py
@@ -1,11 +1,10 @@
-from flask import Flask, request, redirect, jsonify
-from flask_jwt_extended import create_access_token, JWTManager, get_jwt_identity, jwt_required
-import sqlite3, uuid, hashlib, random
-from werkzeug.security import generate_password_hash, check_password_hash
+import sqlite3
 
+# установление соединения с базой данных
 connect = sqlite3.connect('database.db', check_same_thread=False)
 cursor = connect.cursor()
 
+# создание таблиц в базе данных
 cursor.execute('''CREATE TABLE IF NOT EXISTS "users" (
 	"id"	INTEGER NOT NULL,
 	"login"	TEXT NOT NULL,
@@ -26,6 +25,7 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS "links" (
 connect.commit()
 
 
+# добавление нового пользователя в базу данных
 def database_register(login, password):
     res = cursor.execute('SELECT login, password FROM users WHERE login=? AND password=?', (login, password)).fetchone()
     if res is None:
@@ -34,20 +34,24 @@ def database_register(login, password):
         return True
 
 
+# проверка хэшированного пароля
 def password_from_db(login):
-    return cursor.execute('SELECT password FROM users WHERE login=? ', (login, )).fetchone()[0]
+    return cursor.execute('SELECT password FROM users WHERE login=? ', (login,)).fetchone()[0]
 
 
+# сверка введенного логина пользователя с его логином из базы данных
 def check_user_is_correct(login):
-    return cursor.execute('SELECT login FROM users WHERE login=?', (login, )).fetchone()
+    return cursor.execute('SELECT login FROM users WHERE login=?', (login,)).fetchone()
 
 
+# добавление новой ссылки в базу данных
 def add_new_link(full_link, short_link, access, login):
     cursor.execute('INSERT INTO links(full_link, short_link, access, redirect_count, user) VALUES (?, ?, ?, 0, ?)',
                    (full_link, short_link, access, login))
     connect.commit()
 
 
+# вывод списка ссылок пользователя
 def get_list_user_links(login):
     res = cursor.execute('SELECT short_link FROM links WHERE user = ?', (login,)).fetchall()
     name_links = ''
@@ -56,16 +60,31 @@ def get_list_user_links(login):
     return name_links
 
 
+# удаление выбранной ссылки из базы данных
 def del_link_from_db(login, short_link):
     cursor.execute('DELETE FROM links WHERE user = ? AND short_link = ?', (login, short_link))
     connect.commit()
 
 
+# обновление имени выбранной ссылки
 def update_link_from_db(login, new_link, old_link):
     cursor.execute('UPDATE links SET short_link = ? WHERE user = ? AND short_link = ?', (new_link, login, old_link))
     connect.commit()
 
 
+# обновление уровня доступа выбранной ссылки
+def update_access_from_db(login, link, new_access):
+    cursor.execute('UPDATE links SET access = ? WHERE user = ? AND short_link = ?', (new_access, login, link))
+    connect.commit()
+
+
+# вывод уровня доступа ссылки
+def check_access(short_link):
+    res = cursor.execute('SELECT access FROM links WHERE short_link = ?', (short_link,)).fetchone()
+    return res[0]
+
+
+# подсчет количества переходов по конкретной ссылки
 def redirect_db(short_link):
     count = cursor.execute('SELECT redirect_count FROM links WHERE short_link = ?', (short_link,)).fetchone()
     res = int(count[0]) + 1
@@ -73,3 +92,10 @@ def redirect_db(short_link):
     connect.commit()
     return res
 
+
+# проверка на владение конкретной ссылки пользователем
+def check_owner_link(login, short_link):
+    res = cursor.execute('SELECT user, short_link FROM links WHERE user=? AND short_link=?',
+                         (login, short_link)).fetchone()
+    if res is not None:
+        return True
